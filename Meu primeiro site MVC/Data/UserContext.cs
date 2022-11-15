1173274a5bf73c318acdb5b1b87848afa3979df5
xxfau <xxfau@BEN-10-LAPTOP>
2022-11-15 20:57:15 -0300

    Add project files.

---
+++ Meu primeiro site MVC/Data/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Web_App_example.Models;
+
+namespace Web_App_example.Data
+{
+    public class UserContext : DbContext 
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+          
+        }
+
+        public DbSet<User>? User { get; set; }
+
+
+    }
+}
---
+++ Meu primeiro site MVC/Models/User.cs
@@ -0,0 +1,49 @@
+﻿using KeyAttribute = System.ComponentModel.DataAnnotations.KeyAttribute;
+
+namespace Web_App_example.Models
+{
+    public class User
+    {
+
+        public User()
+        {
+
+            IsAdmin = false;
+            Created = DateTime.Now;
+
+
+
+        }
+
+
+
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        public Boolean IsAdmin { get; set; }
+
+        public Byte[]? Photo { get; set; }
+
+        public string? FavoriteColor { get; set; }
+
+
+        public string Gender { get; set; }
+        public string? Profession { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime Created { get; }
+    }
+
+
+
+    
+}
+
